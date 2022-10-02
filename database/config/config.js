2eb9a7428146787c8264e7417516ddc78f90942a
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2022-10-02 19:22:55 -0300

    first deploy

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,23 @@
 module.exports={
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "db_clob",
-    "host": "127.0.0.1",
+    "username": "fpzs_fermin",
+    "password": "sH##%!Elsfy*HugH",
+    "database": "fpzs_db_fermin",
+    "host": "179.61.219.130",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "db_clob",
-    "host": "127.0.0.1",
+    "username": "fpzs_fermin",
+    "password": "sH##%!Elsfy*HugH",
+    "database": "fpzs_db_fermin",
+    "host": "179.61.219.130",
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.USERNAME,
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
-    "host": process.env.HOST,
+    "username": "fpzs_fermin",
+    "password": "sH##%!Elsfy*HugH",
+    "database": "fpzs_db_fermin",
+    "host": "179.61.219.130",
     "dialect": "mysql"
   }
 }
