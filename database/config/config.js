7b036f80604e692093c9aabc0a472706573c4bd3
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2022-11-02 21:29:52 -0300

    add db and title

--- database/config/config.js
+++ database/config/config.js
@@ -1,45 +1,24 @@
 module.exports={
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "db_idear",
-    "host": "127.0.0.1",
+    "username": "b6a566056962bc",
+    "password": "a089a25f",
+    "database": "heroku_f44fa16657d6fad?reconnect=true",
+    "host": "us-cdbr-east-06.cleardb.net",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "db_idear",
-    "host": "127.0.0.1",
+    "username": "b6a566056962bc",
+    "password": "a089a25f",
+    "database": "heroku_f44fa16657d6fad?reconnect=true",
+    "host": "us-cdbr-east-06.cleardb.net",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "db_idear",
-    "host": "127.0.0.1",
+    "username": "b6a566056962bc",
+    "password": "a089a25f",
+    "database": "heroku_f44fa16657d6fad?reconnect=true",
+    "host": "us-cdbr-east-06.cleardb.net",
     "dialect": "mysql"
   }
 }
-/* ,
-  "production": {
-    "username": process.env.USERNAME,
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
-    "host": process.env.HOST,
-    "dialect": "mysql"
-
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "db_clob",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-
-  "production": {
-    "username": "fpzs_fermin",
-    "password": "sH##%!Elsfy*HugH",
-    "database": "fpzs_db_fermin",
-    "host": "179.61.219.130",
-    "dialect": "mysql"
-  } */
+/* mysql://b6a566056962bc:a089a25f@us-cdbr-east-06.cleardb.net/heroku_f44fa16657d6fad?reconnect=true */
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
 
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Idear Server' });
+});
 router.get('/actividades', mainController.list);
 router.post('/actividades', mainController.create);
 router.post('/edit-actividad', mainController.edit)
 router.post('/delete-actividad', mainController.delete)
+
 module.exports = router;
