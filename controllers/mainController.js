5cb52495909a5ba192140f7861f17d28bb6c6877
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2022-11-04 10:41:40 -0300

    crud image

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,5 +1,9 @@
 const db = require('../database/models');
 const { Op } = require('sequelize');
+const { uploadFile, deleteFile, getObjectSignedUrl } = require('../s3.js');
+const sharp = require('sharp');
+const crypto = require('crypto');
+const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex')
 
 module.exports={
   list: (req, res)=>{
@@ -7,6 +11,7 @@ module.exports={
       .then(data => res.status(200).json(data))
   },
   create: (req, res)=>{
+
     db.Actividad.create({
       title: req.body.title,
       startDate: req.body.startDate,
@@ -14,6 +19,7 @@ module.exports={
       allDay: req.body.allDay
     })
       .then(data => res.status(200).json(data))
+
   },
   edit: (req, res)=>{
     db.Actividad.update({...req.body},{where:{id: req.body.id}})
@@ -22,5 +28,68 @@ module.exports={
   delete: (req, res)=>{
     db.Actividad.destroy({where:{id: req.body.id}})
       .then(data => res.status(200).json(data))
+  },
+  addImage: async (req, res)=>{
+    const file = req.file
+    const imageName = generateFileName()
+
+    /* const fileBuffer = await sharp(file.buffer)
+      .resize({ height: 1080, width: 1080, fit: "cover" })
+      .toBuffer() */
+
+    await uploadFile(file.buffer, imageName, file.mimetype)
+
+    await db.Image.create({
+      title: req.body.title,
+      image: imageName,
+      id_category: req.body.id
+    })
+      .then(async data =>{
+        data.dataValues.image = await getObjectSignedUrl(data.dataValues.image)
+        res.status(200).json(data)
+      })
+  },
+  getImages: (req, res) =>{
+    db.Image.findAll({include: [{association: "images"}]})
+    .then(async data =>{
+      for (let item of data){
+        item.dataValues.image = await getObjectSignedUrl(item.dataValues.image)
+      }
+      res.status(200).json(data)
+    })
+  },
+  deleteImage: (req, res)=>{
+    db.Image.findOne({where: {id: req.body.id}})
+      .then(async data =>{
+        let x = data.dataValues.image;
+        await deleteFile(x)
+        db.Image.destroy({where: {id: req.body.id}})
+          .then(a => res.status(200).json({status: "Se borro una imagen correctamente."}))
+      })
+  },
+  addEquipo: async(req, res)=>{
+    const file = req.file
+    const imageName = generateFileName()
+
+    await uploadFile(file.buffer, imageName, file.mimetype)
+
+    await db.Equipo.create({
+      name: req.body.name,
+      image: imageName,
+      linkedin: req.body.linkedin
+    })
+      .then(async data =>{
+        data.dataValues.image = await getObjectSignedUrl(data.dataValues.image)
+        res.status(200).json(data)
+      })
+  },
+  getEquipo: (req, res)=>{
+    db.Equipo.findAll()
+    .then(async data =>{
+      for (let item of data){
+        item.dataValues.image = await getObjectSignedUrl(item.dataValues.image)
+      }
+      res.status(200).json(data)
+    })
   }
 }
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
+const multer = require('multer');
+
+const storage = multer.memoryStorage()
+const upload = multer({storage: storage});
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Idear Server' });
@@ -9,5 +13,10 @@ router.get('/actividades', mainController.list);
 router.post('/actividades', mainController.create);
 router.post('/edit-actividad', mainController.edit)
 router.post('/delete-actividad', mainController.delete)
+router.post('/agregar-imagen', upload.single('file'), mainController.addImage)
+router.get('/images', mainController.getImages)
+router.post('/delete-image', mainController.deleteImage)
+router.post('/equipo', upload.single('file'), mainController.addEquipo)
+router.get('/equipo', mainController.getEquipo)
 
 module.exports = router;
